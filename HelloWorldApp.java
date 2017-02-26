f3222511610aa4aa4b75f37df289846ffcde0d3f
SAC_CS112_Nguyen_Kiet <kietanhnguyen@yahoo.com>
2017-02-26 10:23:12 -0800

    Add files via upload

---
+++ HelloWorldApp.java
@@ -0,0 +1,16 @@
+/*
+ This Program will print out "Hello World"
+ */
+package helloworldapp;
+
+/**
+ *
+ * Written By Kiet Nguyen
+ */
+public class HelloWorldApp {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");    }
+    
+}
